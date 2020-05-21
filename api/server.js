6c1968d0a5afb7286f2b9d3f956f458d8430b6fe
U-THEOPHILUS\Ron <ronald.hughes1982@gmail.com>
2020-05-21 14:00:43 -0400

    added knex

--- api/server.js
+++ api/server.js
@@ -2,4 +2,14 @@ const express = require('express');
 
 const server = express();
 
-const usersRouter =  
+const usersRouter =  require('./router/usersRouter');
+
+server.use(express.json());
+
+server.use('/api', usersRouter)
+
+server.get('/', (req, res) => {
+    res.send('<h3>Welcome to our Family Secrets Recipes API </h3>');
+  });
+ 
+ module.exports = server;
