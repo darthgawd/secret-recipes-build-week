64602aa572a4dbddd7e41dcd67b23d5a825f8330
U-THEOPHILUS\Ron <ronald.hughes1982@gmail.com>
2020-05-22 12:09:49 -0400

    fixed databased schema

--- data/migrations/20200521170231_recipe-tb.js
+++ data/migrations/20200521170231_recipe-tb.js
@@ -7,13 +7,13 @@ exports.up = async function(knex) {
     recipes.string("source").notNullable();
     recipes.text("ingredients").notNullable();
     recipes.text("instructions").notNullable();
-    recipes.string("category").notNullable().unique();
+    recipes.string("category").notNullable();
 
   });
 };
 
 exports.down = async function(knex) {
-    await knex.schema.alterTable("users", (table) =>{
+    await knex.schema.alterTable("recipes", (table) =>{
         table.dropColumn("title");
         table.dropColumn("source");
         table.dropColumn("ingredients");
