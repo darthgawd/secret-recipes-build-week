22c37d4dd9083f657e056443add43cc90ce204a7
U-THEOPHILUS\Ron <ronald.hughes1982@gmail.com>
2020-05-24 14:23:48 -0400

    fixed error?

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ server.use('/api', restricted, recipesRouter)
 server.use('/auth', authRouter)
 
 server.get('/', (req, res) => {
-    res.send('<h3>Welcome to our Family Secrets Recipes API </h3>');
+    res.send('<h3>Welcome to our Family Secrets Recipes</h3>');
   });
  
  module.exports = server;
